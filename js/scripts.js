cf7a83c41404fe920490bc94ed5938a1833f39ef
Andrew Merrell and Brian Dutz <andrew@>
2016-08-29 14:00:06 -0700

    add prototypes find multiple instances of a number and create a round score.

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,14 @@ function Game(numberOfPlayers, players, turn) {
   this.numberOfPlayers = 0;
   this.players = [];
   this.turn = 0;
-}
+};
 
-function Player(round, diceValues, aces, twos, threes, fours, fives, sixes, score) {
+function Player(round, diceValues, diceIndex, countNumber, countMultiple, aces, twos, threes, fours, fives, sixes, score) {
   this.round = round;
   this.diceValues = [];
   this.diceIndex = [];
+  this.countNumber = countNumber;
+  this.countMultiple = 0;
   this.aces = aces;
   this.twos = twos;
   this.threes = threes;
@@ -16,13 +18,40 @@ function Player(round, diceValues, aces, twos, threes, fours, fives, sixes, scor
   this.fives = fives;
   this.sixes = sixes;
   this.score = score;
-}
+};
 
 Player.prototype.roll = function() {
   for (var i = 0; i < this.diceIndex.length; i++) {
     this.diceValues[this.diceIndex[i]] = Math.floor((Math.random() * 6) + 1);
-  }
+  };
+};
+
+Player.prototype.findValues = function() {
+  for (var i = 0; i < this.diceIndex.length; i++) {
+    if (this.diceValues[this.diceIndex[i]] === this.countNumber) {
+      this.countMultiple++;
+    }
+  };
+};
+
+Player.prototype.roundScore = function() {
+  return this.countNumber * this.countMultiple;
+  this.countNumber = 0;
 }
+
+Game.prototype.createPlayers = function(){
+    var myPlayer = new Player();
+    this.players[this.numberOfPlayers] = myPlayer;
+    this.numberOfPlayers++;
+};
+
+Game.prototype.switchTurn = function() {
+  if ((this.turn+1) < this.players.length) {
+    this.turn++;
+  } else{
+    this.turn = 0;
+  };
+};
 
 // UI logic
 $(document).ready(function() {
