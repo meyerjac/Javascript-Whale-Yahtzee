0754d72c0e0f2a0c1725372485abe6c6cda86dc5
Andrew Merrell and Jackson Meyer <andrew@>
2016-08-30 12:58:05 -0700

    reset countMultiple when switching between turns.

--- js/scripts.js
+++ js/scripts.js
@@ -49,6 +49,7 @@ Game.prototype.createPlayers = function(){
 
 Game.prototype.switchTurn = function() {
   this.players[this.turn].diceIndex = [0,1,2,3,4];
+  this.players[this.turn].countMultiple = 0;
   this.players[this.turn].round = 0;
   if ((this.turn) < this.players.length-1) {
     this.turn++;
@@ -59,22 +60,19 @@ Game.prototype.switchTurn = function() {
 };
 
 // UI logic
-
-
-
 $(document).ready(function() {
   var game = new Game();
   for (var i = 0; i < 2; i++) {
       game.createPlayers();
     }
-    var displayDice = function() {
+  var displayDice = function() {
     $("#die1").text(game.players[game.turn].diceValues[0]);
     $("#die2").text(game.players[game.turn].diceValues[1]);
     $("#die3").text(game.players[game.turn].diceValues[2]);
     $("#die4").text(game.players[game.turn].diceValues[3]);
     $("#die5").text(game.players[game.turn].diceValues[4]);
     $("#rollButton").show();
-    }
+  }
 
   $("#rollButton").click(function(){
     game.players[game.turn].roll();
@@ -85,49 +83,46 @@ $(document).ready(function() {
     $("#die3").css("background-color", "inherit");
     $("#die4").css("background-color", "inherit");
     $("#die5").css("background-color", "inherit");
-    console.log(game.players[game.turn].round);
     if (game.players[game.turn].round >= 3) {
       $("#rollButton").hide();
     }
   });
 
-    $("#die1").click(function(){
-      $("#die1").css("background-color", "lightblue");
-      game.players[game.turn].diceIndex.push(0);
-    });
-    $("#die2").click(function(){
-      $("#die2").css("background-color", "lightblue");
-      game.players[game.turn].diceIndex.push(1);
-    });
-    $("#die3").click(function(){
-      $("#die3").css("background-color", "lightblue");
-      game.players[game.turn].diceIndex.push(2);
-    });
-    $("#die4").click(function(){
-      $("#die4").css("background-color", "lightblue");
-      game.players[game.turn].diceIndex.push(3);
-    });
-    $("#die5").click(function(){
-      $("#die5").css("background-color", "lightblue");
-      game.players[game.turn].diceIndex.push(4);
-    });
+  $("#die1").click(function(){
+    $("#die1").css("background-color", "lightblue");
+    game.players[game.turn].diceIndex.push(0);
+  });
+  $("#die2").click(function(){
+    $("#die2").css("background-color", "lightblue");
+    game.players[game.turn].diceIndex.push(1);
+  });
+  $("#die3").click(function(){
+    $("#die3").css("background-color", "lightblue");
+    game.players[game.turn].diceIndex.push(2);
+  });
+  $("#die4").click(function(){
+    $("#die4").css("background-color", "lightblue");
+    game.players[game.turn].diceIndex.push(3);
+  });
+  $("#die5").click(function(){
+    $("#die5").css("background-color", "lightblue");
+    game.players[game.turn].diceIndex.push(4);
+  });
 
-
-
-    $("#useAsAcesButton").click(function(){
-      game.players[game.turn].countNumber = 1;
-      game.players[game.turn].findValues();
-      game.players[game.turn].aces = game.players[game.turn].turnScore()
-      $("#player" + (game.turn+1) + "Aces").append(game.players[game.turn].aces);
-      game.switchTurn();
-      displayDice();
-    });
-    $("#useAsTwosButton").click(function(){
-      game.players[game.turn].countNumber = 2;
-      game.players[game.turn].findValues();
-      game.players[game.turn].twos = game.players[game.turn].turnScore()
-      $("#player" + (game.turn+1) + "Twos").append(game.players[game.turn].twos);
-      game.switchTurn();
-      displayDice();
-    });
+  $("#useAsAcesButton").click(function(){
+    game.players[game.turn].countNumber = 1;
+    game.players[game.turn].findValues();
+    game.players[game.turn].aces = game.players[game.turn].turnScore()
+    $("#player" + (game.turn+1) + "Aces").append(game.players[game.turn].aces);
+    game.switchTurn();
+    displayDice();
+  });
+  $("#useAsTwosButton").click(function(){
+    game.players[game.turn].countNumber = 2;
+    game.players[game.turn].findValues();
+    game.players[game.turn].twos = game.players[game.turn].turnScore()
+    $("#player" + (game.turn+1) + "Twos").append(game.players[game.turn].twos);
+    game.switchTurn();
+    displayDice();
+  });
 });
